7a3bec2d7090726de867cb520cbe3aa9d1ee9691
abdul yusuff <amyusuff1@gmail.com>
2023-04-21 12:26:48 +0000

    Adding even listeners to determine the computers choice, the winner and to update scores

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,41 @@
+/**
+ * Options and scores
+ */
+
+const options = ["rock", "paper", "scissors", "lizard", "spock"];
+let playerScore = 0;
+let computerScore = 0;
+
 /** 
  * function to play the game 
  */
-function play() {
+function play(playerChoice) {
+/**
+ * selects computeres choice
+ */
+    const computerChoice = options[Math.floor(Math.random() * options.length)];
 
 }
+
+/**
+ * To determine the winner and update scores
+ */
+
+if (playerChoice == computerChoice) {
+    document.getElementById("result").innerHTML = "It's a tie!";
+  } else if (
+    (playerChoice == "rock" && (computerChoice == "scissors" || computerChoice == "lizard")) ||
+    (playerChoice == "paper" && (computerChoice == "rock" || computerChoice == "spock")) ||
+    (playerChoice == "scissors" && (computerChoice == "paper" || computerChoice == "lizard")) ||
+    (playerChoice == "lizard" && (computerChoice == "paper" || computerChoice == "spock")) ||
+    (playerChoice == "spock" && (computerChoice == "rock" || computerChoice == "scissors"))
+  ) {
+    document.getElementById("result").innerHTML = "You win!";
+    playerScore++;
+  } else {
+    document.getElementById("result").innerHTML = "Computer wins!";
+    computerScore++;
+  }
 
 /**
  * fuction to restart the game
